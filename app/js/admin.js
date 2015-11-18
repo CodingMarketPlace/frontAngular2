ecf2e084310d05a1ba4ed44037e288ac35f7a01f
DevilBapt <badmib40@hotmail.fr>
2015-11-18 22:35:11 +0100

    admin gestion utilisateur

--- app/js/admin.js
+++ app/js/admin.js
@@ -1,9 +1,38 @@
 var adminApp = angular.module('adminApp', []);
 
-adminApp.controller('AdminController', function ($scope, $location, $routeParams, $http, $rootScope, $cookies) {
+adminApp.controller('AdminController', function ($scope, $location, $routeParams, $http, $rootScope, $cookies, $route) {
     
     $scope.UserManagement = function () {
       $location.path('/admin/user-management');
+    };
+
+    $scope.loadUsersData = function() {
+    	$http.get('http://codingmarketplace.apphb.com/api/Users/All').success(function (data) {
+            $scope.users = data;
+            console.log($scope.users);
+        });
+    };
+
+    $scope.loadUsersData();
+
+    $scope.adminId = "525113112015140402";
+
+    $scope.deleteUser = function(id) {
+    	console.log('http://codingmarketplace.apphb.com/api/Users/Delete/' + $scope.adminId);
+    	console.log('body: userId = ' + id);
+
+    	var userToDelete = {UniqId: id};
+    	var identification = {password: "test", login: "test"};
+
+		$http({ url: 'http://codingmarketplace.apphb.com/api/Users/Delete/' + $scope.adminId, 
+                method: 'DELETE', 
+                data: userToDelete, 
+                headers: {"Content-Type": "application/json;charset=utf-8"}
+        }).success(function(res) {
+        	$route.reload();
+        }, function(error) {
+            console.log(error);
+        });
     };
     
 });
