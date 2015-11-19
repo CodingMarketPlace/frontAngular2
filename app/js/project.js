ff41d48255b47454064c6127734b12777e858460
Hannibal974 <romain.poussin@ynov.com>
2015-11-19 20:09:06 +0100

    réalisation partielle comunication project detail

--- app/js/project.js
+++ app/js/project.js
@@ -1,18 +1,45 @@
 var projectApp = angular.module('projectApp', []);
 
-projectApp.controller('ProjectController', function ($scope, $http, $routeParams, $rootScope, $cookies) {
+projectApp.controller('ProjectController', function ($scope, $http, $routeParams, $rootScope, $cookies, $location, $route) {
     $scope.IdProject = $routeParams.projectId;
-    
+
+    $scope.IdCurrentUser = $cookies.get('user_UniqId');
+
+    $scope.applicants = {};
+    $scope.leaderProject = {};
     $test = $cookies.get('loggedIn');
     $rootScope.loggedIn = ($test === "true");
     console.log($rootScope.loggedIn);
-    
-    $scope.projet = loadProjectDetail ();
-    
+
+    $scope.projet = loadProjectDetail();
+
+    $scope.showLeaderDetail = function () {
+        console.log('iduser : ' + $scope.projet.IdUser);
+        $location.path('user/' + $scope.projet.IdUser);
+    };
+
+    $scope.ApplyProject = function () {
+        $http.post('http://codingmarketplace.apphb.com/api/Projects/Apply/' + $scope.IdCurrentUser, $scope.projet.Id).success(function () {
+            $route.reload();
+        }).error(function () {
+            alert("Une erreur est survenue...");
+        });
+    };
+
     function loadProjectDetail() {
         $http.get('http://codingmarketplace.apphb.com/api/Projects/Detail/' + $scope.IdProject).success(function (data) {
-           console.log('data : ' + data);
-           $scope.projet = data;
+            console.log('data : ' + data);
+            $scope.projet = data;
+            console.log($scope.projet);
+            $http.get('http://codingmarketplace.apphb.com/api/Projects/UsersApplied/' + $scope.IdProject).success(function (data) {
+                $scope.applicants = data;
+                console.log($scope.applicants);
+            });
+            $http.get('http://codingmarketplace.apphb.com/api/Users/Detail/' + $scope.projet.IdUser).success(function (data) {
+                $scope.leaderProject = data;
+            });
+        }).error(function () {
+            alert("Erreur du chargement des postulants au projet.");
         });
     }
 });
