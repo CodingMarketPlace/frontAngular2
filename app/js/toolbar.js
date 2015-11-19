ebc8f34a5f231bf2b72762c2ef234fb3f3f2926b
DevilBapt <badmib40@hotmail.fr>
2015-11-19 22:32:05 +0100

    creation projet + debut image

--- app/js/toolbar.js
+++ app/js/toolbar.js
@@ -110,11 +110,30 @@ toolbarApp.controller('ToolbarCtrl', function ($scope, $rootScope, $mdDialog, $h
         $location.path('user/' + $rootScope.user['UniqId']);
     };
 
+    $scope.uploadFile = function(files) {
+        var fd = new FormData();
+        //Take the first selected file
+        fd.append("file", files[0]);
+
+        $http.post("img/test.jpg", fd, {
+            withCredentials: true,
+            headers: {'Content-Type': undefined },
+            transformRequest: angular.identity
+        }).success(function(data) {
+            console.log("ok");
+        }).error(function() {
+            console.log("not ok");
+        });
+    };
+
     // Création d'un project
     $scope.createProject = function () {
         var project = {ID: 0, Title: $scope.project.projectName, Description: $scope.project.description, Duration: $scope.project.projectDelay, Budget: $scope.project.projectBudget, IdUser: 0, ImageUrl: '', CreationDate: ''};
         
-        $http.post('http://codingmarketplace.apphb.com/api/Projects/Create/', project);
+        $http.post('http://codingmarketplace.apphb.com/api/Projects/Create/' + $rootScope.user['UniqId'], project).success(function(data) {
+            $scope.hide();
+            alert('Le projet a été créé avec succès');
+        });
     };
 
     // Validation des informations d'inscription
