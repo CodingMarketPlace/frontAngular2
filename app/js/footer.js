b2ac4cba9e0479bc45d8f5af2bdd11c95161bf5e
Hannibal974 <romain.poussin@ynov.com>
2015-11-20 14:04:07 +0100

    réindentation + contactUs

--- app/js/footer.js
+++ app/js/footer.js
@@ -1,23 +1,42 @@
 var footerApp = angular.module('footerApp', ['ngMaterial']);
 
-footerApp.controller('FooterCtrl', function ($scope, $mdDialog, $http, $cookies) {
+footerApp.controller('FooterCtrl',
+        function (
+                $scope,
+                $mdDialog,
+                $http)
+        {
 
-    // Affichage  pop-up d'inscription
-    $scope.showDialogContact = function (ev) {
-        $mdDialog.show({
-            controller: DialogController,
-            templateUrl: 'partials/dialog-contact.tmpl.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true
-        })
-                .then(function (answer) {
-                    $scope.status = 'You said the information was "' + answer + '".';
-                }, function () {
-                    $scope.status = 'You cancelled the dialog.';
+            $scope.lastName;
+            $scope.firstName;
+            $scope.email;
+            $scope.content;
+
+            // Affichage  pop-up d'inscription
+            $scope.showDialogContact = function (ev) {
+                $mdDialog.show({
+                    controller: DialogController,
+                    templateUrl: 'partials/dialog-contact.tmpl.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true
+                })
+                        .then(function (answer) {
+                            $scope.status = 'You said the information was "' + answer + '".';
+                        }, function () {
+                            $scope.status = 'You cancelled the dialog.';
+                        });
+            };
+
+            $scope.sendMail = function () {
+                var mail = {FirstName: $scope.firstName, LastName: $scope.lastName, Email: $scope.email, Message: $scope.content};
+                $http.post('http://codingmarketplace.apphb.com/api/Contact/ContactUs', mail).success(function () {
+                    alert('Votre amil a bien été envoyé');
+                    $scope.hide();
                 });
-    };
-});
+            };
+
+        });
 
 // Controller pour l'ouverture des différentes pop-up
 function DialogController($scope, $mdDialog) {
