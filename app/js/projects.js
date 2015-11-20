a7f259f2b1da41c62fb626011f70d1917f5e626c
Eliott Bureau <eliottbureau@gmail.com>
2015-11-20 14:00:46 +0100

    fix indentation

--- app/js/projects.js
+++ app/js/projects.js
@@ -1,25 +1,32 @@
 var projectsApp = angular.module('projectsApp', []);
 
 
-projectsApp.controller('ProjectsController', function ($scope, $routeParams, $http, $rootScope, $cookies) {
-    $scope.searchText = $routeParams.key || '';
+projectsApp.controller('ProjectsController',
+        function (
+                $scope,
+                $routeParams,
+                $http,
+                $rootScope,
+                $cookies)
+        {
+            $scope.searchText = $routeParams.key || '';
 
-    $test = $cookies.get('loggedIn');
-    $scope.countProjects = 0;
-    $scope.user = {};
-    $rootScope.loggedIn = ($test === "true");
+            $test = $cookies.get('loggedIn');
+            $scope.countProjects = 0;
+            $scope.user = {};
+            $rootScope.loggedIn = ($test === "true");
 
-    $scope.projects = loadProjects();
+            $scope.projects = loadProjects();
 
-    function loadProjects() {
-        $http.get('http://codingmarketplace.apphb.com/api/Projects/All/' + $scope.searchText).success(function (data) {
-            $scope.projects = data;
-            $scope.countProjects = $scope.projects.length || $scope.countProjects;
-            angular.forEach($scope.projects, function (projet) {
-                $http.get('http://codingmarketplace.apphb.com/api/Users/Detail/' + projet.IdUser).success(function (data) {
-                   projet.userName = data.FirstName + ' ' + data.LastName;
+            function loadProjects() {
+                $http.get('http://codingmarketplace.apphb.com/api/Projects/All/' + $scope.searchText).success(function (data) {
+                    $scope.projects = data;
+                    $scope.countProjects = $scope.projects.length || $scope.countProjects;
+                    angular.forEach($scope.projects, function (projet) {
+                        $http.get('http://codingmarketplace.apphb.com/api/Users/Detail/' + projet.IdUser).success(function (data) {
+                            projet.userName = data.FirstName + ' ' + data.LastName;
+                        });
+                    });
                 });
-            });
+            }
         });
-    }
-});
--- app/js/user.js
+++ app/js/user.js
@@ -1,101 +1,110 @@
 var userApp = angular.module('userApp', []);
 
-userApp.controller('UserController', function ($scope, $mdDialog, $http, $routeParams, $rootScope, $cookies, $mdMedia, $location) {
+userApp.controller('UserController',
+        function (
+                $scope,
+                $mdDialog,
+                $http,
+                $routeParams,
+                $rootScope,
+                $cookies,
+                $mdMedia,
+                $location)
+        {
 
-    $scope.myAccount = false;
+            $scope.myAccount = false;
 
-    $scope.IdUserConnected = $cookies.get('user_UniqId');
+            $scope.IdUserConnected = $cookies.get('user_UniqId');
 
-    $scope.password = '';
-    $scope.verif_password = '';
+            $scope.password = '';
+            $scope.verif_password = '';
 
-    $scope.screenSmall = $mdMedia('sm');
+            $scope.screenSmall = $mdMedia('sm');
     
-    $rootScope.isDevelopper = $cookies.get('user_Developper') === "true" ? true : false;
+            $rootScope.isDevelopper = $cookies.get('user_Developper') === "true" ? true : false;
     
-    $scope.projects = loadUserProjects();
+            $scope.projects = loadUserProjects();
     
-    function loadUserProjects() {
-      $http.get('http://codingmarketplace.apphb.com/api/Projects/AllForUser/' + $routeParams.userId).success(function(data) {
-        $scope.projects = data;  
-      }).error(function (data) {
-      }); 
-    };
+            function loadUserProjects() {
+                $http.get('http://codingmarketplace.apphb.com/api/Projects/AllForUser/' + $routeParams.userId).success(function(data) {
+                    $scope.projects = data;  
+                }).error(function (data) {
+                }); 
+            };
 
-    $scope.user = {
-        Login: $cookies.get('user_login') || undefined,
-        Password: undefined,
-        Email: undefined,
-        FirstName: undefined,
-        LastName: undefined,
-        verif_password: undefined,
-        Developper: $cookies.get('user_Developper') === "true" ? true : false || undefined,
-        Activated: undefined,
-        Admin: $cookies.get('user_Admin') === "true" ? true : false || undefined,
-        Description: undefined,
-        UniqId: $cookies.get('user_UniqId'),
-        ImageUrl: undefined,
-        ProjectCreator: $cookies.get('user_ProjectCreator') === "true" ? true : false || undefined
-    };
+            $scope.user = {
+                Login: $cookies.get('user_login') || undefined,
+                Password: undefined,
+                Email: undefined,
+                FirstName: undefined,
+                LastName: undefined,
+                verif_password: undefined,
+                Developper: $cookies.get('user_Developper') === "true" ? true : false || undefined,
+                Activated: undefined,
+                Admin: $cookies.get('user_Admin') === "true" ? true : false || undefined,
+                Description: undefined,
+                UniqId: $cookies.get('user_UniqId'),
+                ImageUrl: undefined,
+                ProjectCreator: $cookies.get('user_ProjectCreator') === "true" ? true : false || undefined
+            };      
 
-    $scope.isDisabled = ($scope.IdUserConnected === $routeParams.userId) ? false : true;
-    $scope.loadUserDetail = function () {
-        $http.get('http://codingmarketplace.apphb.com/api/Users/Detail/' + $routeParams.userId).success(function (data) {
-            $scope.user = data;
-            if ($scope.IdUserConnected === $routeParams.userId)
-            {
-                $scope.myAccount = true;
-            }
-        });
-    };
-    $scope.showDialogContact = function (ev) {
-        $mdDialog.show({
-            controller: DialogController,
-            templateUrl: 'partials/dialog-contact.tmpl.html',
-            parent: angular.element(document.body),
-            targetEvent: ev,
-            clickOutsideToClose: true
-        })
-                .then(function (answer) {
+            $scope.isDisabled = ($scope.IdUserConnected === $routeParams.userId) ? false : true;
+            $scope.loadUserDetail = function () {
+                $http.get('http://codingmarketplace.apphb.com/api/Users/Detail/' + $routeParams.userId).success(function (data) {
+                    $scope.user = data;
+                    if ($scope.IdUserConnected === $routeParams.userId)
+                    {
+                        $scope.myAccount = true;
+                    }
+                });
+            };
+            $scope.showDialogContact = function (ev) {
+                $mdDialog.show({
+                    controller: DialogController,
+                    templateUrl: 'partials/dialog-contact.tmpl.html',
+                    parent: angular.element(document.body),
+                    targetEvent: ev,
+                    clickOutsideToClose: true
+                }).then(function (answer) {
                     $scope.status = 'You said the information was "' + answer + '".';
                 }, function () {
                     $scope.status = 'You cancelled the dialog.';
                 });
-    };
+            };      
 
-    $scope.showAlert = function () {
-        // Appending dialog to document.body to cover sidenav in docs app
-        // Modal dialogs should fully cover application
-        // to prevent interaction outside of dialog
-        alert('Vos modifications ont bien été enregistrées..');
-    };
+            $scope.showAlert = function () {
+                // Appending dialog to document.body to cover sidenav in docs app
+                // Modal dialogs should fully cover application
+                // to prevent interaction outside of dialog
+                alert('Vos modifications ont bien été enregistrées..');
+            };      
 
-    $scope.saveModification = function () {
-        if ($scope.password === $scope.verif_password) {
-            var user_modificated = {Id: $scope.user.Id, Email: $scope.user.Email, Password: $scope.password, Description: $scope.user.Description};
-            $http.post('http://codingmarketplace.apphb.com/api/Users/Update/' + $scope.user.UniqId, user_modificated).success(function (data) {
-                $scope.showAlert();
-            });
-        }
-    };
+            $scope.saveModification = function () {
+                if ($scope.password === $scope.verif_password) {
+                    var user_modificated = {Id: $scope.user.Id, Email: $scope.user.Email, Password: $scope.password, Description: $scope.user.Description};
+                    $http.post('http://codingmarketplace.apphb.com/api/Users/Update/' + $scope.user.UniqId, user_modificated).success(function (data) {
+                        $scope.showAlert();
+                    });
+                }
+            };      
 
-    $scope.resetPassword = function () {
-        if ($scope.password === $scope.verif_password) {
-            id = $routeParams.userId;
-            var reset_password = {Password: $scope.password, UniqId: id};
-            $http.post('http://localhost:57396/api/Users/ResetPassword/' + id, reset_password).success(function (data) {
-                alert("Mot de passe changé !");
-                $location.path('/');
-            });
-        };
-    };
+            $scope.resetPassword = function () {
+                if ($scope.password === $scope.verif_password) {
+                    id = $routeParams.userId;
+                    var reset_password = {Password: $scope.password, UniqId: id};
+                    $http.post('http://localhost:57396/api/Users/ResetPassword/' + id, reset_password).success(function (data) {
+                        alert("Mot de passe changé !");
+                        $location.path('/');
+                    });
+                };
+            };      
 
-    $scope.loadUserDetail();
-    $test = $cookies.get('loggedIn');
-    $rootScope.loggedIn = ($test === "true");
-    $scope.currentProjects = undefined;
-    $scope.completedProjects = undefined;
-});
+            $scope.loadUserDetail();
+            $test = $cookies.get('loggedIn');
+            $rootScope.loggedIn = ($test === "true");
+            $scope.currentProjects = undefined;
+            $scope.completedProjects = undefined;
+        });
 //Controller pour l'ouverture des différentes pop-up
 function DialogController($scope, $mdDialog) {
     $scope.hide = function () {
